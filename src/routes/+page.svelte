321ee0283e6a9f040c6342d3de7a153a321a1c3b
Ben Mitchinson <mitchinson.dev@gmail.com>
2025-06-24 21:39:06 -0500

    flex adjustment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <h1>mailboxes</h1>
 <hr class="mt-1 w-18 border-gray-600" />
 
-<div class="mt-8 flex">
+<div class="mt-4 flex flex-wrap gap-16">
 	<LetterBucket name="maeve's mailbox" />
 
 	<LetterBucket name="public mailbox" />
