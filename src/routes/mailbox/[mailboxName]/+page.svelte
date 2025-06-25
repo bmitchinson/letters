85cba740f77c53340c353bcaec0757be9fbbe816
Ben Mitchinson <mitchinson.dev@gmail.com>
2025-06-24 23:27:21 -0500

    okay wout opening anything this is perfect

--- src/routes/mailbox/[mailboxName]/+page.svelte
+++ src/routes/mailbox/[mailboxName]/+page.svelte
@@ -8,11 +8,11 @@
 <h1>{data.mailboxName} mailbox</h1>
 <hr class="mt-1 mb-8 w-18 border-gray-600" />
 
-<div class="flex flex-wrap items-center gap-16">
-	<div class="mb-8 w-xl self-start border border-gray-300 p-6">
+<div class="flex flex-wrap items-center gap-x-16">
+	<div class="mb-8 min-w-sm self-start border border-gray-300 p-6">
 		<SubmitLetterForm />
 	</div>
-	<div class="flex w-2xl flex-grow flex-col items-center gap-4">
+	<div class="flex flex-grow flex-col items-center gap-4">
 		{#each data.mailboxContents as letter}
 			<Letter {letter} />
 		{/each}
